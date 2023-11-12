832f94fb14babbc02584ff61320abbfa4948c009
dali2g <mamejdi12@gmail.com>
2023-11-12 12:59:36 +0100

    initial bot

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const puppeteer = require ("puppeteer");
     const browser = await puppeteer.launch({headless: false});
     
     const page = await browser.newPage()
-    await page.goto("https://www.linkedin.com/in/bader-klila-b60396225/")
+    await page.goto("https://www.linkedin.com")
     await page.setViewport({ width: 1366, height: 768 });
     const title = await page.title()
     const url = await page.url()
